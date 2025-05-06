ded11bdd3b14d238caebd65d098a55b6637bdddf
mwdlave <mwdlave@gmail.com>
2025-05-06 17:07:16 +0200

    fix

--- lpn/src/models/recurrent_lpn.py
+++ lpn/src/models/recurrent_lpn.py
@@ -16,7 +16,7 @@ import numpy as np
 from src.models.transformer import EncoderTransformer, DecoderTransformer
 from src.models.utils import EncoderTransformerConfig, DecoderTransformerConfig
 from src.data_utils import make_leave_one_out
-from src.visualize import display_grid  # Make sure this is properly imported
+#from src.visualize import display_grid  # Make sure this is properly imported
 from src.visualize_grads import trace_recurrent_ga_context, visualize_gradient_computation
 
 
