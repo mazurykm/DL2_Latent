0866fdc8948220eceb8da17d814fa16a6965c954
mwdlave <mwdlave@gmail.com>
2025-05-07 13:00:54 +0200

    fix

--- lpn/src/evaluate_checkpoint.py
+++ lpn/src/evaluate_checkpoint.py
@@ -350,7 +350,7 @@ def evaluate_json(
         key=jax.random.PRNGKey(random_search_seed),
         only_n_tasks=only_n_tasks,  # 'None' to run on all tasks
         progress_bar=True,
-        num_tasks_to_show=5,
+        num_tasks_to_show=0, #change this to produce a plot 
     )
     metrics = {k.split("/")[-1]: v for k, v in metrics.items()}
     metrics["fig"] = fig
