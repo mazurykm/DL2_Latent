f34ddea8634c94bb9a88db6399254f1a26fbef2e
Devin Pereira <devpereira1@gmail.com>
2025-05-07 13:39:14 +0200

    fix import

--- lpn/src/recurrent_train.py
+++ lpn/src/recurrent_train.py
@@ -12,7 +12,6 @@ import jax.numpy as jnp
 import optax
 from jax.tree_util import tree_map
 from matplotlib import pyplot as plt
-optaximport 
 from flax.serialization import from_bytes, msgpack_serialize, to_state_dict
 from flax.training.train_state import TrainState
 import tqdm
