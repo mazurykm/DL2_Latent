74a96acaf10e321a40b971fc91b8ae14bc8cf2a8
Devin Pereira <devpereira1@gmail.com>
2025-05-08 11:26:48 +0200

    Test config without grad log

--- lpn/ARC_train.sh
+++ lpn/ARC_train.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH --partition=gpu_a100
 #SBATCH --gpus=2
-#SBATCH --job-name=ARC_train
+#SBATCH --job-name=grad_flow
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --time=2:00:00
+#SBATCH --time=1:00:00
 #SBATCH --output=/home/scur2570/grad_flow/DL2_Latent/lpn/out/slurm_output_%A.out
 
 module purge
