96b10052293d5b0b52eff0f234f859070c1b67d7
mwdlave <mwdlave@gmail.com>
2025-05-20 16:25:21 +0200

    fix

--- lpn/ARC_eval.sh
+++ lpn/ARC_eval.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 #SBATCH --partition=gpu_a100
-#SBATCH --gpus=1
-#SBATCH --job-name=ARC_eval
+#SBATCH --gpus=2
+#SBATCH --job-name=ARC_eval_22k_baseline
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --time=00:10:00
-#SBATCH --output=/home/scur2570/DL2_Latent/lpn/out/recurrent_eval_save_img_%A.out
+#SBATCH --time=05:00:00
+#SBATCH --output=/home/abaielli/DL2/original-lpn/out/final_eval_arc_22k_baseline_%A.out
 
 module purge
 module load 2023
 module load Anaconda3/2023.07-2
 
-source /home/scur2570/lpn-env/bin/activate
+source /home/abaielli/lpn-env-py39/bin/activate
  
-cd /home/scur2570/DL2_Latent/lpn
+cd /home/abaielli/DL2/original-lpn
+
 export WANDB_API_KEY=8e772263ae8e1722c562169fb1c1b602d2d41d2d
 export HF_TOKEN = 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
 export PYTHONPATH=${PYTHONPATH}:${PWD}
@@ -30,14 +31,44 @@ export PYTHONPATH=${PYTHONPATH}:${PWD}
  # --optimizer-kwargs '{"b2": 0.9}'
 # \
   #--use-product-score true
-
 python src/evaluate_checkpoint.py \
-  -w alisia-baielli/ARC/generous-energy-77--checkpoint:latest\
+  -w alisia-baielli/ARC/decent-bee-38--checkpoint:latest\
   -jc json/arc-agi_evaluation_challenges.json \
   -js json/arc-agi_evaluation_solutions.json \
-  -i matrix \
-  --only-n-tasks 5\
-  --save-intermediate-outputs true
+  -i mean
+
+python src/evaluate_checkpoint.py \
+  -w alisia-baielli/ARC/decent-bee-38--checkpoint:latest\
+  -jc json/arc-agi_evaluation_challenges.json \
+  -js json/arc-agi_evaluation_solutions.json \
+  -i gradient_ascent \
+  --num-steps 1 \
+  --lr 1.0 \
+  --lr-schedule true \
+  --optimizer adam \
+  --optimizer-kwargs '{"b2": 0.9}'
+
+python src/evaluate_checkpoint.py \
+  -w alisia-baielli/ARC/decent-bee-38--checkpoint:latest\
+  -jc json/arc-agi_evaluation_challenges.json \
+  -js json/arc-agi_evaluation_solutions.json \
+  -i gradient_ascent \
+  --num-steps 5 \
+  --lr 1.0 \
+  --lr-schedule true \
+  --optimizer adam \
+  --optimizer-kwargs '{"b2": 0.9}'
+
+python src/evaluate_checkpoint.py \
+  -w alisia-baielli/ARC/decent-bee-38--checkpoint:latest\
+  -jc json/arc-agi_evaluation_challenges.json \
+  -js json/arc-agi_evaluation_solutions.json \
+  -i gradient_ascent \
+  --num-steps 20 \
+  --lr 1.0 \
+  --lr-schedule true \
+  --optimizer adam \
+  --optimizer-kwargs '{"b2": 0.9}'
 
   #--num-steps 2 \
   #--lr 0.1 \
